c2672caf9708c4ebf08c042b96645a71c4d1c8c4
Felipe Barcelos Nogueira <felipenogueira812@gmail.com>
2026-09-16 10:05:06 -0300

    Add modal open/close functionality with event handlers

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,1 +1,27 @@
+export function openModal(modal) {
+  modal.classList.add("popup_is-opened");
 
+  document.addEventListener("keydown", handleEscape);
+}
+
+export function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+
+  document.removeEventListener("keydown", handleEscape);
+}
+
+export function handleOverlayClick(event) {
+  if (event.target === event.currentTarget) {
+    closeModal(event.currentTarget);
+  }
+}
+
+export function handleEscape(event) {
+  if (event.key === "Escape") {
+    const openedPopup = document.querySelector(".popup_is-opened");
+
+    if (openedPopup) {
+      closeModal(openedPopup);
+    }
+  }
+}
